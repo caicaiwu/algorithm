ff32806d43d5c2fecb1bf4461394f46ea94e6283
wupengfei <wupengfei1981@126.com>
2019-07-14 14:10:34 +0800

    增加排序类别参数，升序或降序。

--- src/main/java/com/test/algorithm/sort/InsertSort.java
+++ src/main/java/com/test/algorithm/sort/InsertSort.java
@@ -22,7 +22,7 @@ public class InsertSort {
             data[i] = s;
         }
 
-        insertion_sort(data);
+        insertion_sort(data, 0);
 
         System.out.println(data);
     }
@@ -32,15 +32,27 @@ public class InsertSort {
     * 主循环从第二个数开始往后数，当前下标的数做为准备插入的数key。
     * 次循环从第二个数开始往前数，当发现某个数大于key后，将该数以及
     * 其后面的数都往后挪一位。将key放在某个数的位置。一直查看到最前面。
+    * @ data, 要排序的数据
+    * @ type，排序类型，0升序，1降序
      */
-    public static void insertion_sort(int data[]) {
+    public static void insertion_sort(int data[], int type) {
         int len = data.length;
         for(int j=1; j<len; j++) {
             int i=j-1;
             int key = data[j];
-            while (i>=0 && data[i]>key) {
-                data[i+1] = data[i];
-                i -= 1;
+
+            if(type==0) {
+                // 升序
+                while (i >= 0 && data[i] > key) {
+                    data[i + 1] = data[i];
+                    i -= 1;
+                }
+            } else {
+                // 降序
+                while (i >= 0 && data[i] < key) {
+                    data[i + 1] = data[i];
+                    i -= 1;
+                }
             }
             data[i+1] = key;
         }
