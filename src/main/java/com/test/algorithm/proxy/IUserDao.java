94c76364c8dcf15882f7716f4df2f9e6f2917582
wupengfei <wupengfei1981@126.com>
2019-07-21 08:50:38 +0800

    提交java三种代理方式的demo。

---
+++ src/main/java/com/test/algorithm/proxy/IUserDao.java
@@ -0,0 +1,5 @@
+package com.test.algorithm.proxy;
+
+public interface IUserDao {
+    public void save();
+}
---
+++ src/main/java/com/test/algorithm/proxy/UserDao.java
@@ -0,0 +1,8 @@
+package com.test.algorithm.proxy;
+
+public class UserDao implements IUserDao{
+    @Override
+    public void save() {
+        System.out.println("保存数据");
+    }
+}
